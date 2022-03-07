c3fbc9c76dd242dd50bda09ea818a679ad23048f
vubinhduong <duong261100@gmail.com>
2022-03-07 14:00:38 +0700

    init

---
+++ nuxt/nuxt-test/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="container">
+      <the-hero />
+      <large-card-display
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cards-section="cardInfo"
+      />
+      <small-card-display
+        v-for="cardInfo in smallCardInfo"
+        :key="cardInfo.id"
+        :cards-section="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from '@/assets/data.js'
+export default {
+  data () {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
+    }
+  }
+}
+</script>
+
+<style>
+a {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
